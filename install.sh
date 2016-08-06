0c819b3788d8175ff9c80e90218da22513a33ec8
Michael Dennis <michaeljdennis@gmail.com>
2016-08-06 15:40:01 -0700

    Rename files

--- install.sh
+++ install.sh
@@ -2,21 +2,21 @@
 
 if [ ! -f ~/.bash_profile ]; then
     echo "Linking ~/.bash_profile"
-    ln -s ~/dotfiles/bash_profile ~/.bash_profile
+    ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 else
     echo "~/.bash_profile already exists"
 fi
 
 if [ ! -f ~/.config/git/config ]; then
     echo "Linking ~/.config/git/config"
-    ln -s ~/dotfiles/config/git/config ~/.config/git/config
+    ln -sf ~/dotfiles/.config/git/config ~/.config/git/config
 else
     echo "~/.config/git/config already exists"
 fi
 
 if [ ! -f ~/.config/git/ignore ]; then
     echo "Linking ~/.config/git/ignore"
-    ln -s ~/dotfiles/config/git/ignore ~/.config/git/ignore
+    ln -sf ~/dotfiles/.config/git/ignore ~/.config/git/ignore
 else
     echo "~/.config/git/ignore already exists"
 fi
@@ -30,7 +30,7 @@ fi
 
 if [ ! -f ~/.eslintrc ]; then
     echo "Linking ~/.eslintrc"
-    ln -s ~/dotfiles/eslintrc ~/.eslintrc
+    ln -sf ~/dotfiles/.eslintrc ~/.eslintrc
 else
     echo "~/.eslintrc already exists"
 fi
